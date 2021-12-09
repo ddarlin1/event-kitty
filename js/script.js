15fd144a0402310437d838f4d45a9bc8c247d49d
Dani Darling <87884573+ddarlin1@users.noreply.github.com>
2021-12-08 22:52:46 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.classList.add("disappear");
+    button.innerText = "Wait, come back!";
+  } else {
+    cat.classList.add("show");
+    button.classList.remove("disappear");
+    button.innerText = "Ok, that's enough cuteness.";
+  }
+});
